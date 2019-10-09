149e0ca589c91773773e2ee10567895f4c76a837
Min-Huang <minh.huang96@gmail.com>
2019-10-09 15:31:21 -0700

    added addSteps functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,47 +1,66 @@
-const db = require('../data/db-config.js')
+const db = require("../data/db-config.js");
 
 module.exports = {
-    find,
-    findById,
-    findSteps,
-    add,
-    update,
-    remove,
-}
-
-
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+  addStep
+};
 
 function find() {
-    return db("schemes");
+  return db("schemes");
 }
 
 function findById(id) {
-    return db("schemes").where({ id }).first();
+  return (
+    db("schemes")
+      .where({ id })
+      .first() || null
+  );
 }
 
 function findSteps(schemeId) {
-    return db("steps")
-        .join("schemes", "schemes.id", "steps.scheme_id")
-        .where({ scheme_id: schemeId })
-        .orderBy("step_number").select("scheme_name","step_number","instructions")
-    
+  return db("steps")
+    .join("schemes", "schemes.id", "steps.scheme_id")
+    .where({ scheme_id: schemeId })
+    .orderBy("step_number")
+    .select('steps.id',"scheme_name", "step_number", "instructions");
 }
 
 function add(scheme) {
-    return db("schemes").insert(scheme, "id").then(([id]) => {
-        return findById(id)
-    })
+  return db("schemes")
+    .insert(scheme, "id")
+    .then(([id]) => {
+      return findById(id);
+    });
 }
 
 function update(changes, id) {
-    return db("schemes").where({ id }).update(changes)
-        .then(()=>{
-        return findById(id)
-    })
+  return db("schemes")
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
 }
 
 function remove(id) {
-    
-    return db("schemes").where({ id }).del()
-    
+  return (
+    db("schemes")
+      .where({ id })
+      .del() || null
+  );
+}
+
+function addStep(step, scheme_id) {
+  return db("schemes")
+    .where({ id:scheme_id })
+    .then(() => {
+        return db("steps").insert(step).then(() => {
+          return findSteps(scheme_id);
+      });
+    });
 }
