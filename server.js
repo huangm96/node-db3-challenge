5041cec6fa2cffd2d2c20362410056d87d7180bc
Min-Huang <minh.huang96@gmail.com>
2019-11-20 09:48:50 -0800

    weather api testing 3

--- server.js
+++ server.js
@@ -14,19 +14,39 @@ server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
 server.post('/weather', (req, res) => {
-  console.log(process.env.WEATHER_API_KEY);
-    const api_key = process.env.WEATHER_API_KEY;
-      axios
-        .get(
-          `https://api.darksky.net/forecast/${api_key}/${req.body.latitude},${req.body.longitude}`
-        )
-          .then(response => {
-            //console.log(zipcodes.lookup(90210));
-          res.status(200).json(response.data);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+  const api_key = process.env.WEATHER_API_KEY;
+  const testURL = `https://api.darksky.net/forecast/${api_key}/${req.body.latitude},${req.body.longitude}`;
+  const myInit = {
+    method: "HEAD",
+    mode: "no-cors"
+  };
+
+  const myRequest = new Request(testURL, myInit);
+
+  fetch(myRequest)
+    .then(function(response) {
+      return response;
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(e) {
+      console.log(e);
+    });
+  
+  // console.log(process.env.WEATHER_API_KEY);
+  //   const api_key = process.env.WEATHER_API_KEY;
+  //     axios
+  //       .get(
+  //         `https://api.darksky.net/forecast/${api_key}/${req.body.latitude},${req.body.longitude}`
+  //       )
+  //         .then(response => {
+  //           //console.log(zipcodes.lookup(90210));
+  //         res.status(200).json(response.data);
+  //       })
+  //       .catch(err => {
+  //         console.log(err);
+  //       });
    
 })
 
