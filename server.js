5de6d44f1724fe391d07b37e02dfadbcad8984d1
Min-Huang <minh.huang96@gmail.com>
2019-11-20 09:38:11 -0800

    weather api testing

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
+
+require("dotenv").config();
 const express = require('express');
+const axios = require("axios");
+const cors = require("cors");
+const zipcodes = require("zipcodes");
 
 const SchemeRouter = require('./schemes/scheme-router.js');
 
 const server = express();
+server.use(cors());
 
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
+
+server.post('/weather', (req, res) => {
+  console.log(process.env.WEATHER_API_KEY);
+    const api_key = process.env.WEATHER_API_KEY;
+      axios
+        .get(
+          `https://api.darksky.net/forecast/${api_key}/${req.body.latitude},${req.body.longitude}`
+        )
+          .then(response => {
+            //console.log(zipcodes.lookup(90210));
+          res.status(200).json(response.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+   
+})
 
 module.exports = server;
